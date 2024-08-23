35b8d742ba288f4779744fab0511c350dee7d95b
LucLagrange <128151278+LucLagrange@users.noreply.github.com>
2024-08-23 18:24:33 +0200

    Adjustements to linter action and fixed the script

--- script.py
+++ script.py
@@ -12,7 +12,8 @@ LONGITUDE = os.getenv("LONGITUDE")
 
 # Extracts the current weather
 def get_current_weather(LATITUDE, LONGITUDE, OPEN_WEATHER_MAP_API_KEY):
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={LATITUDE}&lon={LONGITUDE}&appid={OPEN_WEATHER_MAP_API_KEY}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={LATITUDE}&"
+    "lon={LONGITUDE}&appid={OPEN_WEATHER_MAP_API_KEY}"
 
     response = requests.get(url)
 
